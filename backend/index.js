1c366520d005d1198be5b0f6441523649a1a6ea7
Dhruv Pathak <95525900+DhruvGithub414@users.noreply.github.com>
2024-01-06 23:24:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl6kqw5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen("https://instaplace.vercel.app");
     console.log("Connected");
   })
   .catch(err => {
