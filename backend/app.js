62d2e920fd25c8dd138241a106f74d6926697df6
Dhruv Pathak <95525900+DhruvGithub414@users.noreply.github.com>
2024-01-06 17:45:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-
+app.get('/'(req,res)=>{
+  res.json("hello there!!");
+})
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
