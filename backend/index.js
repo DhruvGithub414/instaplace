b76b23666d7e0caec3798a276bba068fec9c659a
Dhruv Pathak <95525900+DhruvGithub414@users.noreply.github.com>
2024-01-06 23:44:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,7 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
-app.get("/",(req,res)=>{
- res.json("hello there!!"); 
-})
+
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
